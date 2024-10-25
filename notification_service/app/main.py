2ee31f712e3f91d81a559f415a2474ad0ae383a7
张艺严 <zhangyiyan@zhangyiyandembp.mynetworksettings.com>
2024-10-25 02:09:54 -0400

    add log

--- notification_service/app/main.py
+++ notification_service/app/main.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from app import models
 from app.database import engine, get_db
 from app.models import notification  # 导入所有模型
-
+from app.config.log import setup_logger
+from app.dependecies.logging_middleware import logging_dependency
 from app.routers.notification_service import notification_router  # 导入通知相关的 router
 
 
@@ -11,8 +12,10 @@ from app.routers.notification_service import notification_router  # 导入通知
 
 notification.Base.metadata.create_all(bind=engine)
 
+logger = setup_logger()
 
 app = FastAPI()
+app.middleware("http")(logging_dependency)
 
 
 # 这是一个router的示例
